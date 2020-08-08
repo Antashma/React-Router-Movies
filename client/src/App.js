c4b246bd19fb9f87caad1859f9711413104f1b09
Samantha Goodman <slgoodman89@gmail.com>
2020-08-07 20:32:35 -0400

    same as last commit was in wrong folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import {Route} from 'react-router-dom'
+import {Route, Switch, Link} from 'react-router-dom'
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import Movie, {} from './Movies/Movie';
 
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -31,8 +31,13 @@ const App = () => {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-      <Route path='/' render={() => {return <MovieList movies = { movieList } />}} />
-      <Route path='/movies/:id' component={ Movie } />
+      
+      <Switch>
+        <Route path='/movie/:id'> 
+          <Movie movies={movieList}/>
+        </Route>
+        <Route path='/' render={() => {return <MovieList movies = { movieList } />}} />
+      </Switch>
     </div>
   );
 };
